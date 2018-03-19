4681d924191b2053dc4fdd0129ba9161dc26dd05
Steve Richards <steve@8balldev.uk>
2018-03-19 15:29:25 +0000

    dont ask questions...

--- scripts/development/install.sh
+++ scripts/development/install.sh
@@ -12,7 +12,7 @@ if [ ! -f "docroot/sites/default/settings.local.php" ]; then
   run_command "Installing default config" "cp docroot/sites/default.settings.local.php docroot/sites/default/settings.local.php"
 fi
 
-run_command "Install Site." "./bin/drush site-install --account-pass=admin $PROFILE $DRUSH_GLOBALS"
+run_command "Install Site." "./bin/drush site-install --account-pass=admin $PROFILE $DRUSH_GLOBALS -y"
 run_command "Resetting the site UUID." "./bin/drush config-set system.site uuid $UUID $DRUSH_GLOBALS"
 run_command "Clearing the cache." "./bin/drush cr -r $ROOT"
 run_command "Creating reference database dump" "./bin/drush -r $ROOT sql-dump --result-file=../$ASSETSDIR/post-install.sql"
