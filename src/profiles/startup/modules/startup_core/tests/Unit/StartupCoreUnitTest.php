9f9bd0f718c973027c036b7f49ccc297f011499a
Steve Richards <steve@8balldev.uk>
2018-10-29 16:43:57 +0000

    adding profile back into start-up repo

---
+++ src/profiles/startup/modules/startup_core/tests/Unit/StartupCoreUnitTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\Tests\startup_core\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Test Unit test.
+ *
+ * @group startup
+ */
+class StartupCoreUnitTest extends UnitTestCase {
+
+  /**
+   * Tests to make sure tests work.
+   */
+  public function testShouldWork() {
+    $this->assertEquals('Test', 'Test');
+  }
+
+}
