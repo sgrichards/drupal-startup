30fe2e0a708d26ec6d119a78cb49e1ce90d3fda3
Steve Richards <steve@8balldev.uk>
2018-10-29 16:36:34 +0000

    fixing broken build on travis

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,7 +10,7 @@ if [ ! -f "$ENV_DOCROOT/sites/default/settings.local.php" ]; then
   run_command "Installing default config" "cp $ENV_DOCROOT/sites/default.settings.local.php $ENV_DOCROOT/sites/default/settings.local.php"
 fi
 
-run_command "Install Site." "./bin/drush site-install --account-pass=admin install_configure_form.enable_update_status_module=NULL $ENV_PROFILE $ENV_DRUSH_GLOBALS -y"
+run_command "Install Site." "./bin/drush site-install $ENV_PROFILE --account-pass=admin install_configure_form.enable_update_status_module=NULL $ENV_DRUSH_GLOBALS -y"
 run_command "Clearing the cache." "./bin/drush cr $ENV_DRUSH_GLOBALS"
 
 echo "Setup complete."
